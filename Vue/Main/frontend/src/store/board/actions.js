157f6d74010a7382c4677dae97b5cf42871ffbf8
JiwonChoi <128335544+cjw0824@users.noreply.github.com>
2023-04-28 13:47:48 +0900

    test

--- Vue/Main/frontend/src/store/board/actions.js
+++ Vue/Main/frontend/src/store/board/actions.js
@@ -1,14 +1,24 @@
-import {
-    REQUEST_BOARD_LIST_TO_SPRING,
-} from './mutation-types'
+import { REQUEST_BOARD_LIST_TO_SPRING } from "./mutation-types";
 
-import axiosInst from '@/utility/axiosInst'
+import axiosInst from "@/utility/axiosInst";
 
 export default {
-    requestBoardListToSpring ({ commit }) {
-        return axiosInst.get('/list')
-            .then((res) => {
-                commit(REQUEST_BOARD_LIST_TO_SPRING, res.data)
-            })
-    }
-}
+  requestBoardListToSpring({ commit }) {
+    return axiosInst.get("/list").then((res) => {
+      commit(REQUEST_BOARD_LIST_TO_SPRING, res.data);
+    });
+  },
+  requestCreateBoardToSpring({}, payload) {
+    const { title, content, writer } = payload;
+
+    return axiosInst
+      .post("/jpa-board/register", { title, content, writer })
+      .then((res) => {
+        alert("게시물 등록 성공!");
+        return res;
+      })
+      .catch(() => {
+        alert("문제 발생!");
+      });
+  },
+};
