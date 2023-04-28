7fe03c488330181736e536a3294006ed3d3e1a77
hanhwack <hanhwack93@gmail.com>
2023-04-28 12:15:30 +0900

    feat: [한확] 상품 리스트UI 생성 [VPP-1]

---
+++ Vue/Main/frontend/src/components/board/BoardListForm.vue
@@ -0,0 +1,46 @@
+<template lang="">
+    <div>
+        <h3>상품 목록</h3>
+        <table>
+            <tr>
+                <th align="center" width="20%">상품명</th>
+                <th align="center" width="20%">제조사</th>
+                <th align="center" width="20%">카테고리</th>
+                <th align="center" width="40%">유통기한</th>
+            </tr>
+            <tr v-if="!boards || (Array.isArray(boards) && boards.length === 0)">
+                <td colspan="4">
+                    등록된 상품이 없습니다
+                </td>
+            </tr>
+            <tr v-else v-for="(board, index) in boards" :key="index">                
+                <td align="center">
+                    {{ board.name }}
+                </td>
+                <td align="center">
+                    {{ board.manufacturer }}
+                </td>
+                <td align="center">
+                    {{ board.category }}
+                </td>
+                <td align="center">
+                    {{ board.expireDate }}
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        boards: {
+            type: Array
+        }
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
--- Vue/Main/frontend/src/views/HomeView.vue
+++ Vue/Main/frontend/src/views/HomeView.vue
@@ -1,15 +1,34 @@
-<template>
-  <hello-world />
+<template lang="">
+  <div>
+      <h2>상품 게시판</h2>
+    
+      <board-list-form :boards="boards"/>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+import { mapActions, mapState } from 'vuex';
+import BoardListForm from '@/components/board/BoardListForm.vue'
+const boardModule = 'boardModule'
+export default {
+  components: { BoardListForm },
+  
+  computed: {
+      ...mapState(boardModule, ['boards']),
+  },
+  mounted () {
+      
+      this.requestBoardListToSpring()
+  },
+  methods: {
+      
+      ...mapActions(
+          boardModule, ['requestBoardListToSpring']
+      )
   }
+}
 </script>
+
+<style lang="">
+  
+</style>
